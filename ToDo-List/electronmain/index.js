dd0ffc385b2388c51007ea5130e496e6a564ea76
heyjoren <heyvaertjoren@gmail.com>
2024-12-01 13:27:36 +0100

    minimum grote van venster
    minWidth: 400
    minHeight: 500

--- ToDo-List/electronmain/index.js
+++ ToDo-List/electronmain/index.js
@@ -9,6 +9,8 @@ const createWindow = () => {
 const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth: 400,
+    minHeight: 500,
     webPreferences: {
         contextIsolation: true,
         preload: path.join(__dirname, 'preload.js')
