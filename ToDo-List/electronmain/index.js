f292e2c0496cac650db465f93a8dfe2b648906b0
heyjoren <heyvaertjoren@gmail.com>
2024-12-13 08:35:58 +0100

    installer werkt en gemaakt
    je moet voor het laten werken van de installer de android map verwijderen in je project.

--- ToDo-List/electronmain/index.js
+++ ToDo-List/electronmain/index.js
@@ -4,6 +4,8 @@ const path = require('path')
 const os = require('os')
 const fs = require('fs');
 
+if(require('electron-squirrel-startup')) return app.quit
+
 const createWindow = () => {
 // Create the browser window.
     const mainWindow = new BrowserWindow({
